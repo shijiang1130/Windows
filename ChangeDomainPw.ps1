9c8d8a560d37393946b9b59d0064b98e772afae2
Linus.yuan <yuan_shijiang@163.com>
2017-11-23 23:14:34 +0800

    Update ChangeDomainPw.ps1

--- ChangeDomainPw.ps1
+++ ChangeDomainPw.ps1
@@ -1,3 +1,17 @@
-```PowerShell
+function changedomainpw($domain)
+{
+param (    
+[System.Security.SecureString]$oldPassword = $( Read-Host -AsSecureString "Old password"),    
+[System.Security.SecureString]$newPassword = $( Read-Host -AsSecureString "New password")
+)
 
-ps>
+$MethodDefinition = @'
+[DllImport("netapi32.dll", CharSet = CharSet.Unicode)]
+public static extern bool NetUserChangePassword(string domainname, string username, string oldPassword, string newPassword);
+'@
+
+$NetAPI32 = Add-Type -MemberDefinition $MethodDefinition -Name 'NetAPI32' -Namespace 'Win32' -PassThru
+$oldPassword = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($oldPassword))
+$newPassword = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($newPassword))
+$NetAPI32::NetUserChangePassword($domain, $env:username, $oldPassword, $newPassword)
+}
